123c8ea97343caeb73cbc6862e36011c32e1d2af
rayenbn <rayen_bn.75@hotmail.fr>
2022-11-13 13:58:37 +0800

    update sitemap

---
+++ resources/views/frontend/sitemap.blade.php
@@ -0,0 +1,82 @@
+@extends('layouts.theme')
+@section('content')
+
+<div class="page-content">	
+    <!-- Breadcrumbs -->
+    <nav aria-label="breadcrumb" class="page-breadcrumb">
+        <div class="container">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="/">Home</a></li>
+                <li class="breadcrumb-item active" aria-current="page">TERMS & CONDITIONS</li>
+            </ol>
+        </div>
+    </nav>				
+    <div class="product-detail">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="product-view">
+                        <div class="product-detail">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-md-2"></div>
+                                        <div class="col-md-8">
+                                            <div class="product-view">
+                                            
+                                            <a href="https://yafu-pet-toys.com/">Home</a>
+                                            <a href="https://yafu-pet-toys.com/login">Login</a>
+                                            <a href="https://yafu-pet-toys.com/cart">Cart</a>
+                                            <a href="https://yafu-pet-toys.com/our-products">Our products</a>
+                                            <a href="https://yafu-pet-toys.com/blogs">Blogs</a>
+                                            <a href="https://yafu-pet-toys.com/contact-us">Contact us</a>
+                                            <a href="https://yafu-pet-toys.com/blogs/1-how-to-import-pet-toys-products-from-china-for-your-amazon-fba-shop">How to import pet toys products from china for your amazon fba shop</a>
+                                            <a href="https://yafu-pet-toys.com/blogs/2-how-to-import-pet-toys-from-china">How to import pet toys from china</a>
+                                            <a href="https://yafu-pet-toys.com/blogs/3-how-to-start-a-pet-toy-business-for-startups-a-customer-conversation-on-how-to-start-a-pet-toys-onlineoffline-business">How to start a pet toy business for startups a customer conversation on how to start a pet toys onlineoffline business</a>
+                                            <a href="https://yafu-pet-toys.com/our-products/2-four-seasons-cat-bed-universal-cat-house-closed-cat-kitten-kitten-bed-winter-kennel-winter-warm-supplies">Four seasons cat bed universal cat house closed cat kitten kitten bed winter kennel winter warm supplies</a>
+                                            <a href="https://yafu-pet-toys.com/our-products/11-massage-tickle-toothbrush-cleaning-turn-around-windmill-cat-toy-turntable-tease-scratching-itching-cat-toys">Massage tickle toothbrush cleaning turn around windmill cat toy turntable tease scratching itching cat toys</a>
+                                            <a href="https://yafu-pet-toys.com/our-products">Our products</a>
+                                           
+                                      
+                                            
+                                            <url>
+                                            <loc>https://yafu-pet-toys.com/our-products/11</loc>
+                                            <lastmod>2022-11-03T03:14:10+00:00</lastmod>
+                                            <priority>0.80</priority>
+                                            </url>
+                                            <url>
+                                            <loc>https://yafu-pet-toys.com/our-products/8</loc>
+                                            <lastmod>2022-11-03T03:14:10+00:00</lastmod>
+                                            <priority>0.80</priority>
+                                            </url>
+                                            <url>
+                                            <loc>https://yafu-pet-toys.com/our-products/6</loc>
+                                            <lastmod>2022-11-03T03:14:10+00:00</lastmod>
+                                            <priority>0.80</priority>
+                                            </url>
+                                            <url>
+                                            <loc>https://yafu-pet-toys.com/about-us</loc>
+                                            <lastmod>2022-11-03T03:14:10+00:00</lastmod>
+                                            <priority>0.80</priority>
+                                            </url>
+                                            <url>
+                                            <loc>https://yafu-pet-toys.com/privacy-policy</loc>
+                                            <lastmod>2022-11-03T03:14:10+00:00</lastmod>
+                                            <priority>0.80</priority>
+                                            </url>
+
+                                        
+                                            </div>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
